c26085e1e1f89d9fd10d5ee0fd2d3c0fa6448221
Selva Logesh R <selvalogesh95@gmail.com>
2025-01-12 01:15:57 +0530

    merge-local-revision-54

--- switch.sh
+++ switch.sh
@@ -6,5 +6,7 @@ git add .
 git commit -m "local-revision-$(git rev-list HEAD --count)"
 home-manager switch --flake ./#$USER -b backup || nix run home-manager/master -- switch --flake ./#$USER -b backup
 git checkout local || git checkout -b local
-git pull origin main:main --ff-only --no-edit
+git pull origin main\:main --ff-only --no-edit
 git merge main -s ort --no-edit
+
+# What is happening
